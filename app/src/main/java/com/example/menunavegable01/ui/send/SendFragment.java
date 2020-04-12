ee178f6fd51a1353228857f4c6a4270b07fc229a
meteorologia <gastonlopez5@gmail.com>
2020-04-12 02:45:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/menunavegable01/ui/send/SendFragment.java
@@ -0,0 +1,33 @@
+package com.example.menunavegable01.ui.send;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import com.example.menunavegable01.R;
+
+public class SendFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+
+        View root = inflater.inflate(R.layout.fragment_send, container, false);
+        Button enviar = root.findViewById(R.id.enviar);
+        enviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(getActivity(),R.id.nav_host_fragment).navigate(R.id.enviado);
+            }
+        });
+        return root;
+    }
+}
